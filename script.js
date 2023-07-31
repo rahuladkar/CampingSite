bd546e753b7ab7dac17cd603a697312d16fe925b
rahuladkar <90685828+rahuladkar@users.noreply.github.com>
2023-07-31 21:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ nextBtn.addEventListener("click", () => {
 function updateCarouselPosition() {
   carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
 }
+
+// carousel
+
+// JavaScript for the carousel section
+
+function openCarousel(imageSrc, title, description) {
+  const modal = document.getElementById("modal");
+  const modalImage = document.getElementById("modal-image");
+  const modalTitle = document.getElementById("modal-title");
+  const modalDescription = document.getElementById("modal-description");
+
+  modalImage.src = imageSrc;
+  modalTitle.textContent = title;
+  modalDescription.textContent = description;
+
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+// Close the modal when clicking outside the modal content
+window.onclick = function (event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    closeModal();
+  }
+};
